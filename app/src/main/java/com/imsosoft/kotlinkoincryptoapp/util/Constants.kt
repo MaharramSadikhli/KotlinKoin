a9bf9d45c091ab1ce18d3d5a62efeccf39ce1354
Muarem <s.muarem10@gmail.com>
2024-03-15 11:44:25 +0400

    created object for constants

---
+++ app/src/main/java/com/imsosoft/kotlinkoincryptoapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.imsosoft.kotlinkoincryptoapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+    const val END_POINT = "MaharramSadikhli/APIs/main/cryptocurrencyapi.json"
+
+}
