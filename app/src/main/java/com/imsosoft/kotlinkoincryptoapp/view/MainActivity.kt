5b430874da76c0a978b8ad8945c1e66367332d7c
Muarem <s.muarem10@gmail.com>
2024-03-15 12:22:37 +0400

    ui design completed

--- app/src/main/java/com/imsosoft/kotlinkoincryptoapp/view/MainActivity.kt
+++ app/src/main/java/com/imsosoft/kotlinkoincryptoapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.imsosoft.kotlinkoincryptoapp
+package com.imsosoft.kotlinkoincryptoapp.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.imsosoft.kotlinkoincryptoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
