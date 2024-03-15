51a032f3fb326874fb2906a9700fe87987711b8f
Muarem <s.muarem10@gmail.com>
2024-03-15 12:49:59 +0400

    created colors

--- app/src/main/java/com/imsosoft/kotlinkoincryptoapp/util/Constants.kt
+++ app/src/main/java/com/imsosoft/kotlinkoincryptoapp/util/Constants.kt
@@ -4,5 +4,13 @@ object Constants {
 
     const val BASE_URL = "https://raw.githubusercontent.com/"
     const val END_POINT = "MaharramSadikhli/APIs/main/cryptocurrencyapi.json"
+    const val BLACK = "#191919"
+    const val BLUE = "#00224D"
+    const val ORANGE = "#FA7D09"
+    const val RED = "#A0153E"
+    const val PURPLE = "#910A67"
+    const val GREEN = "#005B41"
+    const val BROWN = "#321E1E"
+    const val DARK_BLUE = "#03001C"
 
 }
