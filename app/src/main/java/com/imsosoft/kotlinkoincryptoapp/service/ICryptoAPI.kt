1dcf0ffc72741447be6bb427207619a9f8aef321
Muarem <s.muarem10@gmail.com>
2024-03-15 13:48:42 +0400

    service error fixed

--- app/src/main/java/com/imsosoft/kotlinkoincryptoapp/service/ICryptoAPI.kt
+++ app/src/main/java/com/imsosoft/kotlinkoincryptoapp/service/ICryptoAPI.kt
@@ -1,13 +1,13 @@
 package com.imsosoft.kotlinkoincryptoapp.service
 
-import com.imsosoft.kotlinkoincryptoapp.model.CryptoList
+import com.imsosoft.kotlinkoincryptoapp.model.Crypto
 import com.imsosoft.kotlinkoincryptoapp.util.Constants
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ICryptoAPI {
 
-    @GET(Constants.BASE_URL)
-    suspend fun getData(): Response<CryptoList>
+    @GET(Constants.END_POINT)
+    suspend fun getData(): Response<List<Crypto>>
 
 }
