16a4f610cb31e89557676312be6f9973d8bff03d
Muarem <s.muarem10@gmail.com>
2024-03-15 12:56:13 +0400

    added binding

--- app/src/main/java/com/imsosoft/kotlinkoincryptoapp/view/CryptoFragment.kt
+++ app/src/main/java/com/imsosoft/kotlinkoincryptoapp/view/CryptoFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.imsosoft.kotlinkoincryptoapp.R
+import com.imsosoft.kotlinkoincryptoapp.adapter.Adapter
+import com.imsosoft.kotlinkoincryptoapp.adapter.IAdapter
+import com.imsosoft.kotlinkoincryptoapp.databinding.FragmentCryptoBinding
+import com.imsosoft.kotlinkoincryptoapp.model.Crypto
 
 
-class CryptoFragment : Fragment() {
+class CryptoFragment : Fragment(), IAdapter {
 
+    private var _binding: FragmentCryptoBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +29,30 @@ class CryptoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crypto, container, false)
+    ): View {
+        _binding = FragmentCryptoBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = layoutManager
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onItemClicked(crypto: Crypto) {
+        Toast.makeText(requireContext(), "Clicked: ${crypto.currency}", Toast.LENGTH_LONG).show()
     }
 
 }
